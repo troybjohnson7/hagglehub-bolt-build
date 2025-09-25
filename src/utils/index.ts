44db657bd0a0e2e4355c8b683db3ab8f7a857aad
troybjohnson7 <troy.b.johnson@gmail.com>
2025-09-25 18:19:42 -0500

    Fix AddVehicle page routing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,19 @@
 
 
 export function createPageUrl(pageName: string) {
+    // Handle specific page name mappings
+    const pageMap: { [key: string]: string } = {
+        'AddVehicle': '/add-vehicle',
+        'DealDetails': '/deal-details',
+        'EditDeal': '/edit-deal',
+        'EditDealer': '/edit-dealer',
+        'PrivacyPolicy': '/privacy-policy',
+        'TermsOfService': '/terms-of-service'
+    };
+    
+    if (pageMap[pageName]) {
+        return pageMap[pageName];
+    }
+    
     return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
