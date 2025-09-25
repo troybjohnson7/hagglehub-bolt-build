4df7efa8424b0eef0aad6b0dda64f2fc9057c666
troybjohnson7 <troy.b.johnson@gmail.com>
2025-09-25 18:06:20 -0500

    Add back missing navigation elements and fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import './App.css'
-import Pages from "@/pages/index.jsx"
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from '@/pages/Layout'
+import IndexPage from '@/pages/index'
+import AboutPage from '@/pages/About'
+import PrivacyPolicyPage from '@/pages/PrivacyPolicy'
+import TermsOfServicePage from '@/pages/TermsOfService'
+import IndexPage from '@/pages/Index'
+import Dashboard from '@/pages/Dashboard'
+import AddVehiclePage from '@/pages/AddVehicle'
+import MessagesPage from '@/pages/Messages'
+import AccountPage from '@/pages/Account'
+import DealDetailsPage from '@/pages/DealDetails'
+import EditDealPage from '@/pages/EditDeal'
+import EditDealerPage from '@/pages/EditDealer'
+import OnboardingPage from '@/pages/onboarding'
 import { Toaster } from "@/components/ui/toaster"
 
 function App() {
   return (
-    <>
-      <Pages />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout currentPageName="Index"><IndexPage /></Layout>} />
+        <Route path="/about" element={<Layout currentPageName="About"><AboutPage /></Layout>} />
+        <Route path="/privacy-policy" element={<Layout currentPageName="PrivacyPolicy"><PrivacyPolicyPage /></Layout>} />
+        <Route path="/terms-of-service" element={<Layout currentPageName="TermsOfService"><TermsOfServicePage /></Layout>} />
+        <Route path="/dashboard" element={<Layout currentPageName="Dashboard"><Dashboard /></Layout>} />
+        <Route path="/add-vehicle" element={<Layout currentPageName="AddVehicle"><AddVehiclePage /></Layout>} />
+        <Route path="/messages" element={<Layout currentPageName="Messages"><MessagesPage /></Layout>} />
+        <Route path="/account" element={<Layout currentPageName="Account"><AccountPage /></Layout>} />
+        <Route path="/deal-details" element={<Layout currentPageName="DealDetails"><DealDetailsPage /></Layout>} />
+        <Route path="/edit-deal" element={<Layout currentPageName="EditDeal"><EditDealPage /></Layout>} />
+        <Route path="/edit-dealer" element={<Layout currentPageName="EditDealer"><EditDealerPage /></Layout>} />
+        <Route path="/onboarding" element={<Layout currentPageName="Onboarding"><OnboardingPage /></Layout>} />
+      </Routes>
       <Toaster />
-    </>
+    </Router>
   )
 }
 
